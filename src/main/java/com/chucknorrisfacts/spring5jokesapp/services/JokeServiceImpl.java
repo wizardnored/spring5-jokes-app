8f2c2dcff3628e3a3350bcc2bb6af26d2068b839
Nored <antoinederon@gmail.com>
2021-05-24 19:31:55 +0200

    Create Template to show joke and correct JokeService

--- src/main/java/com/chucknorrisfacts/spring5jokesapp/services/JokeServiceImpl.java
+++ src/main/java/com/chucknorrisfacts/spring5jokesapp/services/JokeServiceImpl.java
@@ -1,13 +1,16 @@
 package com.chucknorrisfacts.spring5jokesapp.services;
 
+import org.springframework.stereotype.Service;
+
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
+@Service
 public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
-	this.chuckNorrisQuotes = chuckNorrisQuotes;
+    public JokeServiceImpl() {
+	this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
     @Override
